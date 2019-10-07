d5404e9bb3e66be6ca70ffd622c951f370d4464e
Lance Albertson <lance@osuosl.org>
2019-10-07 15:10:34 -0700

    Fixes to support fog-openstack-1.x
    fog-openstack-1.x already appends "auth/tokens" so we no longer need to
    do that.
    
    Change-Id: I06ee3681ff8bc90f15b2b0ecbdcaebe475709b31
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- recipes/identity_registration.rb
+++ recipes/identity_registration.rb
@@ -34,7 +34,7 @@ admin_project = node['openstack']['identity']['admin_project']
 admin_domain = node['openstack']['identity']['admin_domain_name']
 service_domain_name = node['openstack']['telemetry']['conf']['keystone_authtoken']['user_domain_name']
 connection_params = {
-  openstack_auth_url:     "#{auth_url}/auth/tokens",
+  openstack_auth_url:     auth_url,
   openstack_username:     admin_user,
   openstack_api_key:      admin_pass,
   openstack_project_name: admin_project,
