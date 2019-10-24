27de143b2688f5b77f5b1acc202399b903a8e5d8
Lance Albertson <lance@osuosl.org>
2019-10-24 09:45:25 -0700

    Manage /etc/ceilometer/polling.yaml
    This adds management of /etc/ceilometer/polling.yaml which defines which metrics
    to poll and how often. This pulls in the default configuration which the same
    for both RHEL and Ubuntu and adds two new attributes for configuring the file.
    
    In addition, restart various services related to the configuration file.
    
    Change-Id: I69ed0a8933a971d9cac20dbd4133197a7e4274af
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- recipes/common.rb
+++ recipes/common.rb
@@ -103,3 +103,14 @@ template node['openstack']['telemetry']['conf_file'] do
     service_config: ceilometer_conf_options
   )
 end
+
+template ::File.join(node['openstack']['telemetry']['conf_dir'], 'polling.yaml') do
+  source 'polling.yaml.erb'
+  owner node['openstack']['telemetry']['user']
+  group node['openstack']['telemetry']['group']
+  mode 0o0640
+  variables(
+    interval: node['openstack']['telemetry']['polling']['interval'],
+    meters: node['openstack']['telemetry']['polling']['meters']
+  )
+end
